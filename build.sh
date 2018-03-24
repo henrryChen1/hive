5a9e0867eccf8287ed30d244facf0f608f8dd011
hebaoning <baoning.he@ecreditpal.com>
2018-03-24 16:16:36 +0800

    Add build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-mvn clean package -DskipTests -Phadoop-2,dist -pl hplsql -am
+#mvn clean package -DskipTests -Phadoop-2,dist -pl hplsql -am && cp hplsql/target//hive-hplsql-3.0.0-SNAPSHOT.jar hplsql-0.3.31/
+mvn package -DskipTests -Phadoop-2,dist -pl hplsql -am && cp hplsql/target//hive-hplsql-3.0.0-SNAPSHOT.jar hplsql-0.3.31/
